76ba3d1ebf91c2a88e36e0d7d49be7a52ab27fec
Mohirbek <mahercoder@gmail.com>
2022-03-12 10:16:26 +0500

    Some changes

--- app/actions/commands/start.js
+++ app/actions/commands/start.js
@@ -10,12 +10,20 @@ module.exports = {
             ...ctx.from
         })
 
-        ctx.scene.enter('user-home')
+        const isSubscribed = await BotSettigs.isSubscribed(ctx)
+    
+        if(isSubscribed){
+            ctx.scene.enter('user-home')
+        } else {
+            ctx.scene.enter('subscription')
+        }
+
+        // ctx.scene.enter('user-home')
 
         // Users.get(ctx.from.id)
         // .then( async user => {
         //     if(user){
-        //         ctx.session.LANG = user.language
+        //         ctx.session.LANG = 'uz'
 
         //         if(BotSettigs.isOwner(ctx.from.id) || BotSettigs.isAdmin(ctx.from.id)){
         //             ctx.scene.enter('user-home')
@@ -30,18 +38,7 @@ module.exports = {
         //         }
         //     } else {
         //         ctx.session.LANG = 'uz'
-        //         // Kimdir referal orqali ro'yxatdan o'tayotgan bo'lsa, 
-        //         // referal id olinadi
-        //         const referalId = ctx.message.text ? ctx.message.text.split(' ')[1] : undefined
-        //         if(referalId){
-        //             Users.get(referalId)
-        //             .then( async user => {
-        //                 if(user){ ctx.session.referalId = referalId }
-        //             })
-        //         }
-                
-        //         ctx.scene.enter('signup')
-                
+        //         ctx.scene.enter('signup')   
         //     }
         // })
     }
--- app/index.js
+++ app/index.js
@@ -3,6 +3,7 @@ const { Constants } = require('./tools')
 const { middlewares, commands, updates, startup } = require('./actions')
 
 // const TOKEN = `5021272526:AAHDOwFxI4N0Sl3V8gOg-FutyZmKKhsh0vw`
+
 const TOKEN = `5111219626:AAE1p8p0kh1wFccJq-Tmf0b9VVLUVx3ktkc`
 // 5174234463
 // const TOKEN = Constants.TEST_TOKEN
